c9442dd75e6e0f6ecc47c165c98aac1e8b495447
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-06-28 12:46:32 +0200

    fix: actually allow overriding remotes
    ability to override the current branch's remote, which is useful for
    getting permalinks that use "origin", and not the temporary fork.
    
    ```lua
    require('gitlinker').get_buf_range_url('n', {remote = "origin"})
    require('gitlinker').get_buf_range_url('n', {remote = "fork"})
    ```

--- lua/gitlinker.lua
+++ lua/gitlinker.lua
@@ -51,7 +51,7 @@ local function get_buf_range_url_data(mode, user_opts)
     return nil
   end
   mode = mode or "n"
-  local remote = git.get_branch_remote() or user_opts.remote
+  local remote = user_opts.remote or git.get_branch_remote()
   local repo_url_data = git.get_repo_data(remote)
   if not repo_url_data then
     return nil
@@ -138,7 +138,7 @@ function M.get_repo_url(user_opts)
   user_opts = vim.tbl_deep_extend("force", opts.get(), user_opts or {})
 
   local repo_url_data = git.get_repo_data(
-    git.get_branch_remote() or user_opts.remote
+    user_opts.remote or git.get_branch_remote()
   )
   if not repo_url_data then
     return nil
