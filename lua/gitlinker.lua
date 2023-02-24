7fbc7db6db426d7103045cf1f078d8eff13ad7cf
linrongbin16 <linrongbin16@outlook.com>
2023-02-24 23:30:46 +0800

    remove useless methods

--- lua/gitlinker.lua
+++ lua/gitlinker.lua
@@ -66,24 +66,25 @@ local DEFAULTS = {
 
 local opts = {}
 
---- Setup plugin option and key mapping
+--- Setup configs
 function M.setup(configs)
   opts = vim.tbl_deep_extend("force", DEFAULTS, configs or {})
+  log.debug("[setup] opts: %s", vim.inspect(opts))
   log.setup(opts.debug, opts.console_log, opts.file_log, opts.file_log_name)
   keys.setup(opts.mapping)
 end
 
-local function get_buf_range_url_data(user_opts)
+local function make_linker_data(user_opts)
   local git_root = git.root_path()
-  log.debug("[get_buf_range_url_data] git_root: %s", vim.inspect(git_root))
+  log.debug("[make_linker_data] git_root: %s", vim.inspect(git_root))
   if not git_root then
     log.error("Error! Not in a git repository")
     return nil
   end
-  local remote = user_opts.remote or git.get_branch_remote()
+  local remote = user_opts.remote or git.branch_remote()
   -- local repo_url_data = git.get_repo_data(remote)
   -- log.debug(
-  --   "[init.get_buf_range_url_data] remote: %s, repo_url_data: %s",
+  --   "[make_linker_data] remote: %s, repo_url_data: %s",
   --   vim.inspect(remote),
   --   vim.inspect(repo_url_data)
   -- )
@@ -102,7 +103,7 @@ local function get_buf_range_url_data(user_opts)
 
   local buf_repo_path = util.relative_path(git_root)
   log.debug(
-    "[get_buf_range_url_data] buf_repo_path: %s, git_root: %s",
+    "[make_linker_data] buf_repo_path: %s, git_root: %s",
     vim.inspect(buf_repo_path),
     vim.inspect(git_root)
   )
@@ -116,7 +117,7 @@ local function get_buf_range_url_data(user_opts)
   end
 
   local buf_path = util.relative_path()
-  log.debug("[get_buf_range_url_data] buf_path: %s", vim.inspect(buf_path))
+  log.debug("[make_linker_data] buf_path: %s", vim.inspect(buf_path))
   if git.has_file_changed(buf_path, rev) then
     log.info(
       "Computed Line numbers are probably wrong because '%s' has changes",
@@ -196,7 +197,7 @@ function M.get_buf_range_url(user_opts)
   user_opts = vim.tbl_deep_extend("force", opts, user_opts or {})
   log.debug("[get_buf_range_url] user_opts2: %s", vim.inspect(user_opts))
 
-  local url_data = get_buf_range_url_data(user_opts)
+  local url_data = make_linker_data(user_opts)
   if not url_data then
     log.warn("Warn! No remote found in git repository '%s'!", git.root_path())
     return
--- lua/gitlinker/git.lua
+++ lua/gitlinker/git.lua
@@ -5,12 +5,12 @@ local path = require("plenary.path")
 local log = require("gitlinker.log")
 
 -- wrap the git command to do the right thing always
-local function git(args, cwd)
+local function cmd(args, cwd)
   local output
   local p = job:new({
     command = "git",
     args = args,
-    cwd = cwd or M.root_path(),
+    cwd = cwd or M.get_root(),
   })
   p:after_success(function(j)
     output = j:result()
@@ -19,32 +19,32 @@ local function git(args, cwd)
   return output or {}
 end
 
-local function remote()
-  return git({ "remote" })
+local function get_remote()
+  return cmd({ "remote" })
 end
 
-local function get_remote_uri(remote)
+local function get_remote_url(remote)
   assert(remote, "remote cannot be nil")
-  return git({ "remote", "get-url", remote })[1]
+  return cmd({ "remote", "get-url", remote })[1]
 end
 
 local function get_rev(revspec)
-  return git({ "rev-parse", revspec })[1]
+  return cmd({ "rev-parse", revspec })[1]
 end
 
 local function get_rev_name(revspec)
-  return git({ "rev-parse", "--abbrev-ref", revspec })[1]
+  return cmd({ "rev-parse", "--abbrev-ref", revspec })[1]
 end
 
-function M.is_file_in_rev(file, revspec)
-  if git({ "cat-file", "-e", revspec .. ":" .. file }) then
+local function is_file_in_rev(file, revspec)
+  if cmd({ "cat-file", "-e", revspec .. ":" .. file }) then
     return true
   end
   return false
 end
 
-function M.has_file_changed(file, rev)
-  if git({ "diff", rev, "--", file })[1] then
+local function has_file_changed(file, rev)
+  if cmd({ "diff", rev, "--", file })[1] then
     return true
   end
   return false
@@ -52,7 +52,7 @@ end
 
 local function is_rev_in_remote(revspec, remote)
   assert(remote, "remote cannot be nil")
-  local output = git({ "branch", "--remotes", "--contains", revspec })
+  local output = cmd({ "branch", "--remotes", "--contains", revspec })
   for _, rbranch in ipairs(output) do
     if rbranch:match(remote) then
       return true
@@ -64,108 +64,108 @@ end
 local allowed_chars = "[_%-%w%.]+"
 
 -- strips the protocol (https://, git@, ssh://, etc)
-local function strip_protocol(uri, errs)
-  local protocol_schema = allowed_chars .. "://"
-  local ssh_schema = allowed_chars .. "@"
+-- local function strip_protocol(uri, errs)
+--   local protocol_schema = allowed_chars .. "://"
+--   local ssh_schema = allowed_chars .. "@"
+--
+--   local stripped_uri = uri:match(protocol_schema .. "(.+)$")
+--     or uri:match(ssh_schema .. "(.+)$")
+--   if not stripped_uri then
+--     table.insert(
+--       errs,
+--       string.format(
+--         ": remote uri '%s' uses an unsupported protocol format",
+--         uri
+--       )
+--     )
+--     return nil
+--   end
+--   return stripped_uri
+-- end
 
-  local stripped_uri = uri:match(protocol_schema .. "(.+)$")
-      or uri:match(ssh_schema .. "(.+)$")
-  if not stripped_uri then
-    table.insert(
-      errs,
-      string.format(
-        ": remote uri '%s' uses an unsupported protocol format",
-        uri
-      )
-    )
-    return nil
-  end
-  return stripped_uri
-end
+-- local function strip_dot_git(uri)
+--   return uri:match("(.+)%.git$") or uri
+-- end
 
-local function strip_dot_git(uri)
-  return uri:match("(.+)%.git$") or uri
-end
+-- local function strip_uri(uri, errs)
+--   local stripped_uri = strip_protocol(uri, errs)
+--   return strip_dot_git(stripped_uri)
+-- end
 
-local function strip_uri(uri, errs)
-  local stripped_uri = strip_protocol(uri, errs)
-  return strip_dot_git(stripped_uri)
-end
+-- local function parse_host(stripped_uri, errs)
+--   local host_capture = "(" .. allowed_chars .. ")[:/].+$"
+--   local host = stripped_uri:match(host_capture)
+--   if not host then
+--     table.insert(
+--       errs,
+--       string.format(": cannot parse the hostname from uri '%s'", stripped_uri)
+--     )
+--   end
+--   return host
+-- end
 
-local function parse_host(stripped_uri, errs)
-  local host_capture = "(" .. allowed_chars .. ")[:/].+$"
-  local host = stripped_uri:match(host_capture)
-  if not host then
-    table.insert(
-      errs,
-      string.format(": cannot parse the hostname from uri '%s'", stripped_uri)
-    )
-  end
-  return host
-end
+-- local function parse_port(stripped_uri, host)
+--   assert(host)
+--   local port_capture = allowed_chars .. ":([0-9]+)[:/].+$"
+--   return stripped_uri:match(port_capture)
+-- end
 
-local function parse_port(stripped_uri, host)
-  assert(host)
-  local port_capture = allowed_chars .. ":([0-9]+)[:/].+$"
-  return stripped_uri:match(port_capture)
-end
+-- local function parse_repo_path(stripped_uri, host, port, errs)
+--   assert(host)
+--
+--   local pathChars = "[~/_%-%w%.%s]+"
+--   -- base of path capture
+--   local path_capture = "[:/](" .. pathChars .. ")$"
+--
+--   -- if port is specified, add it to the path capture
+--   if port then
+--     path_capture = ":" .. port .. path_capture
+--   end
+--
+--   -- add parsed host to path capture
+--   path_capture = allowed_chars .. path_capture
+--
+--   -- parse repo path
+--   local repo_path = stripped_uri
+--     :gsub("%%20", " ") -- decode the space character
+--     :match(path_capture)
+--     :gsub(" ", "%%20") -- encode the space character
+--   if not repo_path then
+--     table.insert(
+--       errs,
+--       string.format(": cannot parse the repo path from uri '%s'", stripped_uri)
+--     )
+--     return nil
+--   end
+--   return repo_path
+-- end
 
-local function parse_repo_path(stripped_uri, host, port, errs)
-  assert(host)
+-- local function parse_uri(uri, errs)
+--   local stripped_uri = strip_uri(uri, errs)
+--
+--   local host = parse_host(stripped_uri, errs)
+--   if not host then
+--     return nil
+--   end
+--
+--   local port = parse_port(stripped_uri, host)
+--
+--   local repo_path = parse_repo_path(stripped_uri, host, port, errs)
+--   if not repo_path then
+--     return nil
+--   end
+--
+--   -- do not pass the port if it's NOT a http(s) uri since most likely the port
+--   -- is just an ssh port, so it's irrelevant to the git permalink construction
+--   -- (which is always an http url)
+--   if not uri:match("https?://") then
+--     port = nil
+--   end
+--
+--   return { host = host, port = port, repo = repo_path }
+-- end
 
-  local pathChars = "[~/_%-%w%.%s]+"
-  -- base of path capture
-  local path_capture = "[:/](" .. pathChars .. ")$"
-
-  -- if port is specified, add it to the path capture
-  if port then
-    path_capture = ":" .. port .. path_capture
-  end
-
-  -- add parsed host to path capture
-  path_capture = allowed_chars .. path_capture
-
-  -- parse repo path
-  local repo_path = stripped_uri
-      :gsub("%%20", " ") -- decode the space character
-      :match(path_capture)
-      :gsub(" ", "%%20") -- encode the space character
-  if not repo_path then
-    table.insert(
-      errs,
-      string.format(": cannot parse the repo path from uri '%s'", stripped_uri)
-    )
-    return nil
-  end
-  return repo_path
-end
-
-local function parse_uri(uri, errs)
-  local stripped_uri = strip_uri(uri, errs)
-
-  local host = parse_host(stripped_uri, errs)
-  if not host then
-    return nil
-  end
-
-  local port = parse_port(stripped_uri, host)
-
-  local repo_path = parse_repo_path(stripped_uri, host, port, errs)
-  if not repo_path then
-    return nil
-  end
-
-  -- do not pass the port if it's NOT a http(s) uri since most likely the port
-  -- is just an ssh port, so it's irrelevant to the git permalink construction
-  -- (which is always an http url)
-  if not uri:match("https?://") then
-    port = nil
-  end
-
-  return { host = host, port = port, repo = repo_path }
-end
-
-function M.get_closest_remote_compatible_rev(remote)
+local function get_closest_remote_compatible_rev(remote)
   -- try upstream branch HEAD (a.k.a @{u})
   local upstream_rev = get_rev("@{u}")
   if upstream_rev then
@@ -204,44 +204,40 @@ function M.get_closest_remote_compatible_rev(remote)
   return nil
 end
 
-function M.get_repo_data(remote)
-  local errs = {
-    string.format("Failed to retrieve repo data for remote '%s'", remote),
-  }
-  local remote_uri = get_remote_uri(remote)
-  if not remote_uri then
-    table.insert(
-      errs,
-      string.format(": cannot retrieve url from remote '%s'", remote)
-    )
-    return nil
-  end
+-- local function get_repo_data(remote)
+--   local errs = {
+--     string.format("Failed to retrieve repo data for remote '%s'", remote),
+--   }
+--   local remote_uri = get_remote_url(remote)
+--   if not remote_uri then
+--     table.insert(
+--       errs,
+--       string.format(": cannot retrieve url from remote '%s'", remote)
+--     )
+--     return nil
+--   end
+--
+--   local repo = parse_uri(remote_uri, errs)
+--   if not repo or vim.tbl_isempty(repo) then
+--     log.error("Error! %s", table.concat(errs))
+--   end
+--   return repo
+-- end
 
-  local repo = parse_uri(remote_uri, errs)
-  if not repo or vim.tbl_isempty(repo) then
-    log.error("Error! %s", table.concat(errs))
-  end
-  return repo
-end
-
-function M.get_remote_url(remote)
-  return get_remote_uri(remote)
-end
-
-function M.root_path()
+local function get_root()
   local buf_path = path:new(vim.api.nvim_buf_get_name(0))
   local current_folder = tostring(buf_path:parent())
-  local root = git({ "rev-parse", "--show-toplevel" }, current_folder)[1]
+  local root = cmd({ "rev-parse", "--show-toplevel" }, current_folder)[1]
   log.debug("[git.root] current_folder:%s, root:%s", current_folder, root)
   return tostring(path:new(root))
 end
 
-function M.get_branch_remote()
+local function get_branch_remote()
   -- origin/upstream
-  local remotes = remote()
+  local remotes = get_remote()
 
   if #remotes == 0 then
-    log.error("Error! Git repo '%s' has no remote", M.root_path())
+    log.error("Error! Git repo '%s' has no remote", M.get_root())
     return nil
   end
 
@@ -257,7 +253,7 @@ function M.get_branch_remote()
 
   -- origin
   local remote_from_upstream_branch =
-      upstream_branch:match("^(" .. allowed_chars .. ")%/")
+    upstream_branch:match("^(" .. allowed_chars .. ")%/")
 
   if not remote_from_upstream_branch then
     log.error(
@@ -280,5 +276,12 @@ function M.get_branch_remote()
   )
   return nil
 end
+
+M.get_root = get_root
+M.get_remote_url = get_remote_url
+M.is_file_in_rev = is_file_in_rev
+M.has_file_changed = has_file_changed
+M.get_closest_remote_compatible_rev = get_closest_remote_compatible_rev
+M.get_branch_remote = get_branch_remote
 
 return M
